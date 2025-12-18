66e1318db631c56089f6e0f694b3a0fd0ea9f012
周敬彬 <1@itmeow.com>
2025-12-18 18:41:27 +0800

    fix: center main image and force modal image to 80vw/80vh

--- src/app/[locale]/(landing)/_social-highlights/components/PostDetail.tsx
+++ src/app/[locale]/(landing)/_social-highlights/components/PostDetail.tsx
@@ -66,7 +66,7 @@ export const PostDetail: React.FC<PostDetailProps> = ({ post, relatedPosts, onBa
           {/* Left Column - Image */}
           <div className="flex flex-col gap-6">
             {/* 1:1 Image Container adapting to viewport height */}
-            <div className="flex justify-start w-full">
+            <div className="flex justify-center w-full">
                 <div 
                     className="relative rounded-[24px] p-1 border border-gray-200 dark:border-border shadow-lg bg-white dark:bg-card cursor-pointer hover:shadow-xl transition-shadow aspect-square w-full"
                     style={{ maxWidth: 'calc(100vh - 250px)' }}
@@ -216,7 +216,7 @@ export const PostDetail: React.FC<PostDetailProps> = ({ post, relatedPosts, onBa
                 <img
                     src={selectedImage}
                     alt="Enlarged view"
-                    className="w-auto h-auto max-w-[95vw] max-h-[95vh] object-contain pointer-events-auto"
+                    className="w-[80vw] h-[80vh] object-contain pointer-events-auto"
                 />
                 )}
             </div>
